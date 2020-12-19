32474b033c874012cb3d4347af362ada95faaf64
wswapped <wswapped@gmail.com>
2020-12-19 12:58:19 +0200

    Add sprite section

--- pages/index.vue
+++ pages/index.vue
@@ -23,12 +23,57 @@
 		</div>
 		
 	</div>
+	<!-- Offsets for absolute position -->
 	<div class="home-banner-after"></div>
-	<v-container>
-		<v-row>
+	<v-container class="section">
+		<v-row class="pl-15">
 			<v-col cols="12" >
-				<h1 class="primary--text">TIME-LAPSE & PROJECT MANAGEMENT CAMERAS</h1>
+				<h1 class="section-title primary--text">TIME-LAPSE & PROJECT MANAGEMENT CAMERAS</h1>
 			</v-col>
+			<v-col cols="4" >
+				<div>
+					<span class="sprite-icon icon-promote"></span>
+					<h2 class="text-uppercase">Promote</h2>
+				</div>
+				
+				<p>
+					Showcase your work with construction time-lapse videos and live streaming.
+				</p>
+				<ul class="check-list">
+					<li>Build brand exposure</li>
+					<li>Reach a wider audience</li>
+					<li>Enhance marketing/sales</li>
+				</ul>
+			</v-col>
+			<v-col cols="4" >
+				<div>
+					<span class="sprite-icon icon-collaborate"></span>
+					<h2 class="text-uppercase">COLLABORATE</h2>
+				</div>
+				<p>
+					Empower Project Managers with the right image at the right time.
+				</p>
+				<ul class="check-list">
+					<li>Share progress</li>
+					<li>Have live & immediate access</li>
+					<li>Access anywhere</li>
+				</ul>
+			</v-col>
+			<v-col cols="4" >
+				<div>
+					<span class="sprite-icon icon-avoid-disputes"></span>
+				</div>
+				<h2 class="text-uppercase">AVOID DISPUTES</h2>
+				<p>
+					Don't just win arguments, avoid them. You own the evidence.
+				</p>
+				<ul class="check-list">
+					<li>Own the evidence</li>
+					<li>Share footage</li>
+					<li>Entire project duration recorded</li>
+				</ul>
+			</v-col>
+			
 		</v-row>
 	</v-container>
 </div>
