8e223f175101c19147ae60de6be5c39c84c2f441
wswapped <wswapped@gmail.com>
2020-12-30 06:48:00 +0200

    Add project structure

---
+++ pages/projects/_project_slug.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <div class="page-guide grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="6">
+            <p class="text-h5">{{ project.name }}</p>
+          </v-col>
+          <v-col cols="6">
+            <p class="h3 float-right">
+              <span class="primary--text">Home</span> >
+              <span class="primary-text">Projects</span> >
+              <span>{{ project.name }}</span>
+            </p>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
+    <v-container class="">
+      <v-row class="pl-15 section">
+        <v-col cols="12">
+          <h2 class="text-center">Video</h2>
+        </v-col>
+        <v-col cols="8">
+          <p>
+            <strong>The Palms at Cardinal</strong> is a residential development
+            in Beaumont Texas. Check out this construction timelapse capturing
+            the progress being made by OneForce Construction.
+          </p>
+
+          <p>
+            Located just minutes away from
+            <strong>Lamar University and central Beaumont</strong>, this
+            development will be ideal for both students and working
+            professionals.
+          </p>
+
+          <p>
+            Upon completion this project will offer 1-4 bedroom homes, as well
+            as study rooms, office space, a 24 hour gym & a beautiful outdoor
+            area with a modern pool.
+          </p>
+          <v-img
+            :alt="project.name"
+            :src="project.img"
+            :lazy-src="project.img"
+            class="img-fluid"
+          ></v-img>
+        </v-col>
+        <v-col cols="4">
+          <h3>Project Details</h3>
+          <v-simple-table>
+            <template v-slot:default>
+              <tbody>
+                <tr>
+                  <td>Client</td>
+                  <td><a href="https://oneforcetx.com/">OneForce Construction</a></td>
+                </tr>
+                <tr>
+                  <td>Channel Partner</td>
+                  <td><a href="https://www.danners.com/">Danner's Inc</a></td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+import { vueVimeoPlayer } from "vue-vimeo-player";
+import ContactForm from "~/components/ContactForm.vue";
+import "~/assets/scss/projects.scss";
+
+export default {
+  head: {
+    title: "Projects Archive",
+  },
+  data() {
+    return {
+      project: {
+        name: "The Palms - Beaumont, Texas",
+        description:
+          "The Palms at Cardinal is a residential development in Beaumont Texas. Check out this construction timelapse capturing the progress being",
+        img: "/img/pro1.png",
+      },
+    };
+  },
+  components: {
+    Logo,
+    VuetifyLogo,
+    vueVimeoPlayer,
+  },
+};
+</script>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -20,14 +20,17 @@
           cols="3"
           v-for="(customer, custKey) in happyCustomers"
           :key="custKey"
-		  class="project"
+          class="project"
         >
-          <v-img
-            :alt="customer.name"
-            :src="customer.img"
-            :lazy-src="customer.img"
-            class="img-fluid"
-          ></v-img>
+          <NuxtLink :to="`/projects/${getSlug(customer.name)}`">
+            <v-img
+              :alt="customer.name"
+              :src="customer.img"
+              :lazy-src="customer.img"
+              class="img-fluid"
+              to="projects/test"
+            ></v-img>
+          </NuxtLink>
           <h2 class="primary--text">{{ customer.name }}</h2>
           <p>{{ customer.description }}</p>
         </v-col>
@@ -47,6 +50,15 @@ export default {
   head: {
     title: "Projects Archive",
   },
+  methods: {
+    getSlug(projectName) {
+      // Returns route friendly url
+      let ret = projectName.toString().toLowerCase();
+      ret = ret.replace(" ", "_");
+      ret = ret.replace(/\W/gi, "");
+      return ret;
+    },
+  },
   data() {
     return {
       happyCustomers: [
@@ -57,18 +69,21 @@ export default {
           img: "/img/pro1.png",
         },
         {
-		  name: "ZED PODs - Bristol",
-		  description: 'Despite concerns about air quality for potential tenants, Bristol councilors have approved proposals for “peculiar” apartments in a Bristol car',
+          name: "ZED PODs - Bristol",
+          description:
+            "Despite concerns about air quality for potential tenants, Bristol councilors have approved proposals for “peculiar” apartments in a Bristol car",
           img: "/img/zedpods-bristol-ft.gif",
         },
         {
-		  name: "Simonds Homes",
-		  description: 'Simonds Homes has built an enviable reputation throughout our history for designing the finest homes in Australia. Their mission is',
+          name: "Simonds Homes",
+          description:
+            "Simonds Homes has built an enviable reputation throughout our history for designing the finest homes in Australia. Their mission is",
           img: "/img/Aspire-Element_181112_164340.jpg",
         },
         {
-		  name: "Bartra Poplar Row",
-		  description: 'This site was acquired by Bartra on Poplar Road, comprising 0.14ha/0.35acres, zoned Z4 (District Centre) with the objective: ‘to have',
+          name: "Bartra Poplar Row",
+          description:
+            "This site was acquired by Bartra on Poplar Road, comprising 0.14ha/0.35acres, zoned Z4 (District Centre) with the objective: ‘to have",
           img: "/img/poplar1-469x364-1.png",
         },
       ],
